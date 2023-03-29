3572ca851ce0adbfc2eba44fddc31fb8d6e69b98
Gbare <gbareoyekan@yahoo.com>
2023-03-29 11:40:10 +0100

    fix legal page error

--- src/pages/Widgets/TextEditor/index.js
+++ src/pages/Widgets/TextEditor/index.js
@@ -1,32 +1,17 @@
 import React from "react";
-import { useEffect } from "react";
-import { Parser } from "html-to-react";
 
 import style from "./style.module.css";
-import { useRef } from "react";
-import { useCallback } from "react";
+
 
 const TextEditor = React.forwardRef(({ text, plain }, ref) => {
-  const textRef = useRef();
-  useEffect(() => {
-    const htmlToReactParser = new Parser();
-    const ReactElement = htmlToReactParser.parse(text);
-    textRef.current = ReactElement;
-  }, []);
-
-  useEffect(() => {
-    // console.log("current -->", textRef?.current);
-  }, [ref?.current]);
-
   return (
     <div className={plain ? style.plain : `c`}>
       <div className={plain ? undefined : style.section}>
         <div
           id="text-editor"
           className={style.text}
-          // dangerouslySetInnerHTML={{ __html: text }}
+          dangerouslySetInnerHTML={{ __html: text }}
         >
-          {textRef.current ? <div ref={ref}> {textRef.current} </div> : null}
         </div>
       </div>
     </div>
